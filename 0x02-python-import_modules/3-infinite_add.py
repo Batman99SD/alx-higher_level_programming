9eed733edd0a38fdb706a99ebd08529e35537d7f
Batman99SD <azozkrg1@gmail.com>
2023-11-30 16:38:37 -0800

    3 edit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
+from sys import argv
 if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
-
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
+    count = len(argv)
+    add = 0
+    if count == 1:
+        print("{}".format(add))
     else:
-        print("{} arguments:".format(i))
-
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+        for i in range(1, count):
+            add += int(argv.__getitem__(i))
+        print("{}".format(add))
